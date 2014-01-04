e076f60b7336c0aebd6387265881f25226a94d4a
Shenjiaqi <Shenjiaqi@whatbecome@163.com>
2014-01-04 12:57:01 +0800

    1057 got ac, several wa

--- 1057/1057.cpp
+++ 1057/1057.cpp
@@ -1,31 +1,40 @@
-#include <cstdio>
 #include <iostream>
+#include <cassert>
 using namespace std;
 typedef long long ll;
 ll x,y,k,b;
-int ans;
-void dfs(ll v,int cnt,ll val,int left)
+ll dp[37][2][21];
+ll que(ll v)
 {
-  if(cnt==0)
-    {
-      if(v>=x)
-	++ans;
-    }
-  else if((ll)v+(ll)val<=y && left>=cnt && v+c[left][cnt]>=x)
-    {
-      dfs(v+val,cnt-1,val*b,left-1);
-      dfs(v,cnt,val*b,left-1);
-    }
+  int tmp[40],ti=0;
+  for(;v;v/=b)
+    tmp[ti++]=v%b;
+  int kk=k;
+  ll r=0;
+  for(int i=ti-1;i>=0 && kk>=0;--i)
+    if(tmp[i]==1)
+      r+=dp[i][0][kk],--kk;
+    else if(tmp[i]>1)
+      {
+	r+=dp[i][1][kk];
+	break;
+      }
+  return r;
 }
 int main()
 {
   cin>>x>>y>>k>>b;
-  int m=0;
-  for(ll v=1;v<=y;++m)
-    v*=b;
-  for(int i=0;i<m;++i)
-    for(int j=0;j<
-  dfs(0,k,1,m);
-  printf("%d",ans);
+  dp[0][0][0]=dp[0][1][0]=1;
+  dp[0][1][1]=1;
+  for(int i=1;i<37;++i)
+    {
+      dp[i][0][0]=dp[i][1][0]=1;
+      for(int j=1;j<=k;++j)
+	{
+	  dp[i][0][j]=dp[i-1][1][j];
+	  dp[i][1][j]=dp[i][0][j]+dp[i-1][1][j-1];
+	}
+    }
+  cout<<que(y+1)-que(x)<<endl;
   return 0;
 }
